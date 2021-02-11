d1822acb083a9ed8cf463efcca03efdcb1cf947a
Thang Nguyen <tnguye20@uvm.edu>
2021-02-10 19:07:21 -0500

    Add functions for triggers and cloud functions

---
+++ functions/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+const functions = require("firebase-functions");
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send('Hello from the land of Thang');
+});
+//# sourceMappingURL=index.js.map
